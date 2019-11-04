e8232c8291b2ad77b77129a0f2ca657995a60b68
Lubos Horacek <lubos@iflix.com>
2019-11-04 18:13:23 +0100

    Second fragment bind

--- app/src/main/java/com/generalbytes/myapplication/fragment/SecondFragment.kt
+++ app/src/main/java/com/generalbytes/myapplication/fragment/SecondFragment.kt
@@ -1,12 +1,23 @@
 package com.generalbytes.myapplication.fragment
 
+import androidx.fragment.app.Fragment
 import com.generalbytes.myapplication.R
 import com.generalbytes.myapplication.databinding.FragmentBinding
+import com.generalbytes.myapplication.di.FragmentScope
 import com.generalbytes.myapplication.vm.FragmentViewModel
+import dagger.Binds
+import dagger.Module
 import javax.inject.Inject
 
 
 class SecondFragment: BaseFragment<FragmentBinding, FragmentViewModel>(R.layout.fragment) {
     @Inject lateinit var vm: FragmentViewModel
     override fun createViewModel() = vm
+
+    @Module
+    abstract class InjectionModule {
+        @Binds
+        @FragmentScope
+        internal abstract fun bindFragment(fragment: SecondFragment): Fragment
+    }
 }
